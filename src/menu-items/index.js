9184f2775191485d20e4c7cfca67434ae675e5d1
Suliman Khan <sulimank418@gmail.com>
2022-12-17 11:33:33 +0500

    Drawer set

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -8,11 +8,12 @@ import other from './other';
 import pages from './pages';
 import financials from './financials';
 import support from './support';
+import applications from './applications';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [accounts, showroom, production, financials, support, formsTables, chartsMap, pages, other]
+  items: [accounts, showroom, production, financials, support, applications, formsTables, chartsMap, pages, other]
 };
 
 export default menuItems;
